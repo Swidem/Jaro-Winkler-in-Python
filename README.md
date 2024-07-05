16c2e1af90ac01aeb286a544a1804374ab7f0bc6
M Swiderek <mswiderek@student.agh.edu.pl>
2024-07-05 11:14:46 +0200

    create file with mistakes

--- README.md
+++ README.md
@@ -1,14 +1,11 @@
 # Jaro-Winkler alghoritm in Python
 
-While using the book „Pan Tadusz” in txt format I am doing these tasks:
+While using the book "Pan Tadeusz" in txt format, I am performing the following tasks:
 
-1.    Splitting text form file to write one word per line and eliminating stoplist
-
-2.    Creating new file with 50 words and the second one with modifications in words.
-
-3.    Creating Jaro-Winkler Similarity comparing words from file with well-written words to the second one with modifications.
-
-4.    Building process which will extract compared words from out text file to those used in file corrections.txt for our similarity level (alpha).
+1. Splitting the text from the file to write one word per line, while eliminating words from the stoplist.
+2. Creating a new file with 50 randomly selected words and another file with modified words.
+3. Calculating the Jaro-Winkler Similarity by comparing words from the well-written words file to the file with modifications.
+4. Developing a process to extract and compare words from our text file to those used in the correct50words.txt file, based on a specified similarity level (alpha).
 
 Useful link:
 https://en.wikipedia.org/wiki/Jaro%E2%80%93Winkler_distance#Jaro%E2%80%93Winkler_Similarity
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import random
+import os
 
-def main(input_file, output_file):
-    #znaki specjalne
-    punctuations = ['.', ',', '-', ';', '«', ':', '!', '-', '(', ')', '?', '—', '…']
-    #stoplista
+def create_new_files(input_file):
+    #znaki i słowa do eliminacji
+    punctuations = ['.', ',', '-', ';', '«', ':', '!', '-', '(', ')', '?', '—', '…','»', '*']
     stopwords = ['a', 'aby', 'ach', 'acz', 'aczkolwiek', 'aj', 'albo', 'ale', 'ależ', 'ani', 'aż', 'bardziej', 'bardzo', 'bo', 'bowiem', 'by', 'byli', 'bynajmniej', 'być', 'był', 'była', 'było', 'były', 'będzie', 'będą', 'cali', 'cała', 'cały', 'ci', 'cię', 'ciebie', 'co', 'cokolwiek', 'coś', 'czasami', 'czasem', 'czemu', 'czy', 'czyli', 'daleko', 'dla', 'dlaczego', 'dlatego', 'do', 'dobrze', 'dokąd', 'dość', 'dużo', 'dwa', 'dwaj', 'dwie', 'dwoje', 'dziś', 'dzisiaj', 'gdy', 'gdyby', 'gdyż', 'gdzie', 'gdziekolwiek', 'gdzieś', 'i', 'ich', 'ile', 'im', 'inna', 'inne', 'inny', 'innych', 'iż', 'ja', 'ją', 'jak', 'jaka', 'jakaś', 'jakby', 'jaki', 'jakichś', 'jakie', 'jakiś', 'jakiż', 'jakkolwiek', 'jako', 'jakoś', 'je', 'jeden', 'jedna', 'jedno', 'jednak', 'jednakże', 'jego', 'jej', 'jemu', 'jest', 'jestem', 'jeszcze', 'jeśli', 'jeżeli', 'już', 'ją', 'każdy', 'kiedy', 'kilka', 'kimś', 'kto', 'ktokolwiek', 'ktoś', 'która', 'które', 'którego', 'której', 'który', 'których', 'którym', 'którzy', 'ku', 'lat', 'lecz', 'lub', 'ma', 'mają', 'mało', 'mam', 'mi', 'mimo', 'między', 'mną', 'mnie', 'mogą', 'moi', 'moim', 'moja', 'moje', 'może', 'możliwe', 'można', 'mój', 'mu', 'musi', 'my', 'na', 'nad', 'nam', 'nami', 'nas', 'nasi', 'nasz', 'nasza', 'nasze', 'naszego', 'naszych', 'natomiast', 'natychmiast', 'nawet', 'nią', 'nic', 'nich', 'nie', 'niech', 'niego', 'niej', 'niemu', 'nigdy', 'nim', 'nimi', 'niż', 'no', 'o', 'obok', 'od', 'około', 'on', 'ona', 'one', 'oni', 'ono', 'oraz', 'oto', 'owszem', 'pan', 'pana', 'pani', 'po', 'pod', 'podczas', 'pomimo', 'ponad', 'ponieważ', 'powinien', 'powinna', 'powinni', 'powinno', 'poza', 'prawie', 'przecież', 'przed', 'przede', 'przedtem', 'przez', 'przy', 'roku', 'również', 'sama', 'są', 'się', 'skąd', 'sobie', 'sobą', 'sposób', 'swoje', 'ta', 'tak', 'taka', 'taki', 'takie', 'także', 'tam', 'te', 'tego', 'tej', 'temu', 'ten', 'teraz', 'też', 'to', 'tobą', 'tobie', 'toteż', 'trzeba', 'tu', 'tutaj', 'twoi', 'twoim', 'twoja', 'twoje', 'twym', 'twój', 'ty', 'tych', 'tylko', 'tym', 'u', 'w', 'wam', 'wami', 'was', 'wasz', 'wasza', 'wasze', 'we', 'według', 'wiele', 'wielu', 'więc', 'więcej', 'wszyscy', 'wszystkich', 'wszystkie', 'wszystkim', 'wszystko', 'wtedy', 'wy', 'właśnie', 'z', 'za', 'zapewne', 'zawsze', 'ze', 'zł', 'znowu', 'znów', 'został', 'żaden', 'żadna', 'żadne', 'żadnych', 'że', 'żeby']
 
     #odczytanie oryginalnego pliku
@@ -11,9 +11,10 @@ def main(input_file, output_file):
         lines = file.readlines()
 
     cleaned_lines = []
-    
+    base, ext = os.path.splitext(input_file)
+
     #zapisanie w nowym pliku jednego słowna na linię
-    with open(output_file, 'w', encoding='utf-8') as file:
+    with open(f"{base}_copy{ext}", 'w', encoding='utf-8') as file:
         for line in lines:
             if line.strip():
                 for punct in punctuations:
@@ -24,12 +25,45 @@ def main(input_file, output_file):
                     file.write(word.lower() + "\n")
                     cleaned_lines.append(word.lower())
 
+    random50words=[]
     #losowanie 50 słów do nowego pliku
     with open('correct50words.txt', 'w', encoding='utf-8') as file:
         for _ in range(50):
             random_word = random.choice(cleaned_lines)
             file.write(random_word + "\n")
-                
+            random50words.append(random_word)
+
+    #modyfikacja 50 słów do nowego pliku
+    with open('50wordswithmistakes.txt', 'w', encoding='utf-8') as file:
+        
+        typo_types = ['swap', 'drop', 'replace']
+        typo = random.choice(typo_types)
+        
+        for n in range(50):
+            word = random50words[n]        
+            typo = random.choice(typo_types)
+
+            i, j = random.sample(range(len(word)), 2)
+            word = list(word)
+
+            if typo == "swap":
+                word[i], word[j] = word[j], word[i]                
+        
+            elif typo == "drop":
+                word[i] = ""
+
+            elif typo == "replace":
+                word[i], word[j] = word[i], word[i]      
+            
+            word = ''.join(word)
+            file.write(word + "\n")
+
+#def jaro():
+#    return "aaa"
+
+#def jarowinkler():
+
+
+
 input_file = 'pt.txt'
-output_file = 'pt_copy.txt'
-main(input_file, output_file)
+create_new_files(input_file)
